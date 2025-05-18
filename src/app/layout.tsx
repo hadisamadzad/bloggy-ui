2af7534981baff48bf79f3e61b1a8415e6853ff0
Hadi Samadzad <h.samadzad@gmail.com>
2025-05-18 17:51:21 +0100

    Set theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="light">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
