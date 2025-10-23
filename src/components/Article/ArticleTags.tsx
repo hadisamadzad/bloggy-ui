ba14c8d805c98cdd24a14616aa81ae593dbeb9c0
Hadi Samadzad <h.samadzad@gmail.com>
2025-10-23 11:51:26 +0100

    Create tag component

--- src/components/Article/ArticleTags.tsx
+++ src/components/Article/ArticleTags.tsx
@@ -1,3 +1,5 @@
+import Tag from "../Common/Tag";
+
 interface ArticleTagsProps {
   tags: string[];
 }
@@ -6,12 +8,7 @@ export default function ArticleTags({ tags }: ArticleTagsProps) {
   return (
     <div className="flex flex-wrap gap-2 text-base-content/90">
       {tags.map((tag) => (
-        <div
-          key={tag}
-          className="bg-base-content/10 text-label-lg py-2 px-4 rounded-full w-fit hover:bg-base-content/20"
-        >
-          <a href="">{tag}</a>
-        </div>
+        <Tag key={tag} name={tag} />
       ))}
     </div>
   );
